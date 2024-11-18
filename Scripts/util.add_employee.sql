49ebb88808b925266a5b7d9fe870d9e98fd06fc9
ovoitsekhova <ovoitsekhova@gmail.com>
2024-11-18 10:42:06 +0200

    Create util.add_employee.sql

---
+++ Scripts/util.add_employee.sql
@@ -0,0 +1,56 @@
+--Успішно
+BEGIN
+    util.add_employee(p_first_name      => 'Olha',
+                      p_last_name       => 'Voitsekhova',
+                      p_email           => 'ovoitsekhova@gmail.com', 
+                      p_phone_number    => '0938888888',
+                      p_job_id          => 'MK_MAN',
+                      p_salary          => 12000,
+                      p_manager_id      => 201,
+                      p_department_id   => 20);
+
+END;
+/
+
+--Неуспішно 1
+BEGIN
+    util.add_employee(p_first_name      => 'Olha',
+                      p_last_name       => 'Voitsekhova',
+                      p_email           => 'ovoitsekhova@gmail.com', 
+                      p_phone_number    => '0938888888',
+                      p_job_id          => 'MK_MAN',
+                      p_salary          => 12000,
+                      p_manager_id      => 201,
+                      p_department_id   => 2);
+
+END;
+/
+
+--Неуспішно 2
+BEGIN
+    util.add_employee(p_first_name      => 'Olha',
+                      p_last_name       => 'Voitsekhova',
+                      p_email           => 'ovoitsekhova@gmail.com', 
+                      p_phone_number    => '0938888888',
+                      p_job_id          => 'MK_MAN',
+                      p_salary          => 120000,
+                      p_manager_id      => 201,
+                      p_department_id   => 20);
+
+END;
+/
+
+
+--Неуспішно 3
+BEGIN
+    util.add_employee(p_first_name      => 'Olha',
+                      p_last_name       => 'Voitsekhova',
+                      p_email           => 'ovoitsekhova@gmail.com', 
+                      p_phone_number    => '0938888888',
+                      p_job_id          => 'MK_MA',
+                      p_salary          => 12000,
+                      p_manager_id      => 201,
+                      p_department_id   => 20);
+
+END;
+/
