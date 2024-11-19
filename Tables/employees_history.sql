c72f4230ccad588d4a8b28bd04de2d9b5186783c
ovoitsekhova <ovoitsekhova@gmail.com>
2024-11-19 11:12:03 +0200

    Create employees_history.sql

---
+++ Tables/employees_history.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees_history (employee_id       NUMBER,
+                                first_name        VARCHAR2(50),
+                                last_name         VARCHAR2(50),
+                                job_id            VARCHAR2(10),
+                                department_id     NUMBER,
+                                manager_id        NUMBER,
+                                hire_date         DATE,
+                                fire_date         DATE,
+                                reason            VARCHAR2(500));
