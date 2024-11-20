816b3f50d61cd465c9a3767075d915051ea90c0d
ovoitsekhova <ovoitsekhova@gmail.com>
2024-11-20 13:06:06 +0200

    Update util.add_employee.sql

--- Scripts/util.add_employee.sql
+++ Scripts/util.add_employee.sql
@@ -1,41 +1,41 @@
 --Успішно
 BEGIN
-    util.add_employee(p_first_name      => 'Olha',
-                      p_last_name       => 'Voitsekhova',
-                      p_email           => 'ovoitsekhova@gmail.com', 
-                      p_phone_number    => '0938888888',
-                      p_job_id          => 'MK_MAN',
-                      p_salary          => 12000,
-                      p_manager_id      => 201,
-                      p_department_id   => 20);
+    olxga_irn.util.add_employee(p_first_name      => 'Olha',
+                                p_last_name       => 'Voitsekhova',
+                                p_email           => 'ovoitsekhova@gmail.com', 
+                                p_phone_number    => '0938888888',
+                                p_job_id          => 'MK_MAN',
+                                p_salary          => 12000,
+                                p_manager_id      => 201,
+                                p_department_id   => 20);
 
 END;
 /
 
 --Неуспішно 1
 BEGIN
-    util.add_employee(p_first_name      => 'Olha',
-                      p_last_name       => 'Voitsekhova',
-                      p_email           => 'ovoitsekhova@gmail.com', 
-                      p_phone_number    => '0938888888',
-                      p_job_id          => 'MK_MAN',
-                      p_salary          => 12000,
-                      p_manager_id      => 201,
-                      p_department_id   => 2);
+    olxga_irn.util.add_employee(p_first_name      => 'Olha',
+                                p_last_name       => 'Voitsekhova',
+                                p_email           => 'ovoitsekhova@gmail.com', 
+                                p_phone_number    => '0938888888',
+                                p_job_id          => 'MK_MAN',
+                                p_salary          => 12000,
+                                p_manager_id      => 201,
+                                p_department_id   => 2);
 
 END;
 /
 
 --Неуспішно 2
 BEGIN
-    util.add_employee(p_first_name      => 'Olha',
-                      p_last_name       => 'Voitsekhova',
-                      p_email           => 'ovoitsekhova@gmail.com', 
-                      p_phone_number    => '0938888888',
-                      p_job_id          => 'MK_MAN',
-                      p_salary          => 120000,
-                      p_manager_id      => 201,
-                      p_department_id   => 20);
+    olxga_irn.util.add_employee(p_first_name      => 'Olha',
+                                p_last_name       => 'Voitsekhova',
+                                p_email           => 'ovoitsekhova@gmail.com', 
+                                p_phone_number    => '0938888888',
+                                p_job_id          => 'MK_MAN',
+                                p_salary          => 120000,
+                                p_manager_id      => 201,
+                                p_department_id   => 20);
 
 END;
 /
@@ -43,14 +43,14 @@ END;
 
 --Неуспішно 3
 BEGIN
-    util.add_employee(p_first_name      => 'Olha',
-                      p_last_name       => 'Voitsekhova',
-                      p_email           => 'ovoitsekhova@gmail.com', 
-                      p_phone_number    => '0938888888',
-                      p_job_id          => 'MK_MA',
-                      p_salary          => 12000,
-                      p_manager_id      => 201,
-                      p_department_id   => 20);
+    olxga_irn.util.add_employee(p_first_name      => 'Olha',
+                                p_last_name       => 'Voitsekhova',
+                                p_email           => 'ovoitsekhova@gmail.com', 
+                                p_phone_number    => '0938888888',
+                                p_job_id          => 'MK_MA',
+                                p_salary          => 12000,
+                                p_manager_id      => 201,
+                                p_department_id   => 20);
 
 END;
 /
