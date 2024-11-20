dc6edfbef15c019b95bc625846c3cdb37b326854
ovoitsekhova <ovoitsekhova@gmail.com>
2024-11-20 13:06:26 +0200

    Update util.fire_an_employee.sql

--- Scripts/util.fire_an_employee.sql
+++ Scripts/util.fire_an_employee.sql
@@ -1,16 +1,16 @@
 --Успішно
 BEGIN
-    util.fire_an_employee(p_employee_id      => 201);
+    olxga_irn.util.fire_an_employee(p_employee_id      => 201);
 END;
 /
 
 --Неуспішно 
 BEGIN
-    util.fire_an_employee(p_employee_id      => 10);
+    olxga_irn.util.fire_an_employee(p_employee_id      => 10);
 END;
 /
 
 
-SELECT * FROM logs;
+SELECT * FROM olxga_irn.logs;
 
-SELECT * FROM employees_history;
+SELECT * FROM olxga_irn.employees_history;
