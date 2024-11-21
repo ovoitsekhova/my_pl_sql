ee3f8b6a08ad070d1f7baf4e6b8f1a68aec716b8
ovoitsekhova <ovoitsekhova@gmail.com>
2024-11-21 11:08:59 +0200

    Create sys_params.sql

---
+++ Tables/sys_params.sql
@@ -0,0 +1,5 @@
+CREATE TABLE olxga_irn.sys_params (param_name    VARCHAR2(150),
+                                   value_date    DATE,
+                                   value_text    VARCHAR2(2000),
+                                   value_number  NUMBER,
+                                   param_descr   VARCHAR2(200));
