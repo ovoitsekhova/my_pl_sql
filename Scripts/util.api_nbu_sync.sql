101dc9ac0abe60e5f7fdbd2fc69a2c77a6b922c4
ovoitsekhova <ovoitsekhova@gmail.com>
2024-11-23 11:20:07 +0200

    Update util.api_nbu_sync.sql

--- Scripts/util.api_nbu_sync.sql
+++ Scripts/util.api_nbu_sync.sql
@@ -1,6 +1,6 @@
 -заповнення таблиці sys_params параметром list_currencies
-INSERT INTO olxga_irn.sys_params (param_name, value_date, value_text, param_descr)
-VALUES ('list_currencies', SYSDATE, 'USD,EUR,KZT,AMD,GBP,ILS', 'Список валют для синхронізації в процедурі util.api_nbu_sync');
+INSERT INTO olxga_irn.sys_params (param_name, value_text, param_descr)
+VALUES ('list_currencies', 'USD,EUR,KZT,AMD,GBP,ILS', 'Список валют для синхронізації в процедурі util.api_nbu_sync');
 COMMIT;
 
 --ручний виклик процедури
