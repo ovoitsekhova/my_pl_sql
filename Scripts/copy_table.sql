56eea807129735e956858f810da21a20c41b5b3a
ovoitsekhova <ovoitsekhova@gmail.com>
2024-11-23 21:45:00 +0200

    Update copy_table.sql

--- Scripts/copy_table.sql
+++ Scripts/copy_table.sql
@@ -1,8 +1,22 @@
+--вдалий кейс
 DECLARE
     v_err VARCHAR2(100);
 BEGIN 
-    copy_table(p_source_scheme => 'HR',
-               p_list_table => 'REGIONS',
+    copy_table(p_source_scheme => 'hr',
+               p_list_table => 'countries,sales',
+               p_copy_data => TRUE,
+               po_result => v_err);
+    dbms_output.put_line(v_err);
+END;
+/
+
+--невдалий кейс
+DECLARE
+    v_err VARCHAR2(100);
+BEGIN 
+    copy_table(p_source_scheme => 'hr',
+               p_list_table => 'employees',
+               p_copy_data => TRUE,
                po_result => v_err);
     dbms_output.put_line(v_err);
 END;
